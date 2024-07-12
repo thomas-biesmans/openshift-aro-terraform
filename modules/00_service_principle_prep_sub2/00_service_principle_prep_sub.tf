7984b354b164158e1ca9bf4d712a01f0dd5ce8eb
Thomas Biesmans <thomas.biesmans@realdolmen.com>
2024-07-12 10:17:54 +0200

    Added contributor role

--- modules/00_service_principle_prep_sub2/00_service_principle_prep_sub.tf
+++ modules/00_service_principle_prep_sub2/00_service_principle_prep_sub.tf
@@ -19,6 +19,12 @@ resource "azuread_service_principal" "sp_aro" {
   owners    = [data.azuread_client_config.current.object_id]
 }
 
+resource "azurerm_role_assignment" "sp_aro_contributor" {
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.sp_aro.object_id
+}
+
 resource "azuread_service_principal_password" "sp_pw_aro" {
   service_principal_id = azuread_service_principal.sp_aro.object_id
 }
@@ -30,5 +36,6 @@ resource "local_file" "service_principle_details" {
     subscription_id = data.azurerm_client_config.current.subscription_id
     client_id       = azuread_application.app_aro.client_id
     client_secret   = azuread_service_principal_password.sp_pw_aro.value
+    role            = azurerm_role_assignment.sp_aro_contributor.role_definition_id 
   })
 }
