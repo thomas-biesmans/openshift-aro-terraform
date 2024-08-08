03b073115e2dc05a649b26254662353e24c3d998
donisaurs <donisaurs@gmail.com>
2024-08-08 21:11:51 +0200

    Added table creation as part of earlier regression

--- 20_aro_config/modules/20_kasten/initinsert.sql
+++ 20_aro_config/modules/20_kasten/initinsert.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS stock (
+    id SERIAL PRIMARY KEY,
+    product VARCHAR(255),
+    unit VARCHAR(50),
+    amount NUMERIC(10, 2),
+    price NUMERIC(10, 2)
+);
 INSERT INTO stock(product,unit,amount,price) VALUES ('z80','unit',100,2);
 INSERT INTO stock(product,unit,amount,price) VALUES ('6502','unit',100,2);
 INSERT INTO stock(product,unit,amount,price) VALUES ('m68k','unit',100,2);
